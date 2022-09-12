638a5e07075e971322c2801a245205edf69f8d90
René R. Clavijo <reneclavijo@live.com>
2022-09-12 00:21:31 -0500

    Agregando los comentarios para los posts en las recetas separados por usuarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,40 +1,19 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[ show edit update destroy ]
-
-  # GET /comments or /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1 or /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
+  before_action :authenticate_user!
+  before_action :set_comment, only: %i[ update destroy ]
 
   # POST /comments or /comments.json
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment = current_user.comments.create(comment_params)
+    if @comment.save
+      redirect_to @comment.post, notice: "Comment successfully created"
+    else
+      flash[:error] = "Cannot create this comment"
+      redirect_to @comment.post
+    end    
   end
 
-  # PATCH/PUT /comments/1 or /comments/1.json
+  # TODO: Finalizar vista de update comentario
   def update
     respond_to do |format|
       if @comment.update(comment_params)
@@ -47,7 +26,7 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1 or /comments/1.json
+  # TODO: Agregar botón de eliminar comentario en las cartas
   def destroy
     @comment.destroy
 
@@ -65,6 +44,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:text, :user_id, :post_id)
+      params.require(:comment).permit(:text, :post_id)
     end
 end
